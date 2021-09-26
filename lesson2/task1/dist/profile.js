1431a90077bbdce6f1fc3aff777ad17559100474
IURII <yralei@gmail.com>
2021-09-26 17:23:04 +0300

    commit

--- lesson2/task1/dist/profile.js
+++ lesson2/task1/dist/profile.js
@@ -3,5 +3,5 @@ const printProfile = function (profileData) {
     name,
     company
   } = profileData;
-  console.log(`${name} from ${company}`);
+  console.log("".concat(name, " from ").concat(company));
 };
