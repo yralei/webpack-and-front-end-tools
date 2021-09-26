99adbb3d2bf278e731ef14b7732915ba5ef77bdb
IURII <yralei@gmail.com>
2021-09-26 12:05:50 +0300

    first commit

---
+++ lesson1/task1/src/scripts/logger/logger.js
@@ -0,0 +1,14 @@
+export const createLogger = (name) => {
+  const logs = [];
+  return {
+    log(message) {
+      logs.push(`log - ${name} - ${message}`);
+    },
+    error(errorText) {
+      logs.push(`error - ${name} - ${errorText}`);
+    },
+    getLogs() {
+      return logs;
+    },
+  };
+};
