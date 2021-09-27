a14d0c2f8fbada60d4312c77043a2b23351c51b1
IURII <yralei@gmail.com>
2021-09-27 11:30:50 +0300

    commit

---
+++ lesson4/task1/calculator/index.js
@@ -0,0 +1,16 @@
+// экспортируйте sum и mult как именной export
+
+export const sum = (a, b) => {
+  console.log('implementation for sum');
+  // ... implementation
+};
+
+export const mult = (a, b) => {
+  console.log('implementation for mult');
+  // ... implementation
+};
+
+export default (expression) => {
+  console.log('implementation for calc');
+  // ... implementation
+};
---
+++ lesson4/task1/profile/gateway.js
@@ -0,0 +1,10 @@
+// экспортируйте fetchUser как export по умолчанию
+
+export default async function fetchUser(userId) {
+  console.log('implementation for fetchUser');
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (!response.ok) {
+    throw new Error('Failed to get user data');
+  }
+  return await response.json();
+}
