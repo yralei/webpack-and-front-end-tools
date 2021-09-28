8c896ffc78c54b9de67d4f97ef0f99ec09cb2f7f
IURII <yralei@gmail.com>
2021-09-28 23:03:53 +0300

    commit

---
+++ lesson8/task1/src/index.js
@@ -0,0 +1,11 @@
+import renderProfile from './profile/renderProfile';
+import './index.css';
+
+const profileData = {
+  name: 'Tom',
+  location: 'The World',
+};
+
+renderProfile(profileData);
+
+alert('App is ready');
---
+++ lesson8/task2/src/list/createTask.js
@@ -0,0 +1,30 @@
+import { renderTasks } from './renderer.js';
+import { getItem, setItem } from './storage.js';
+import { createTask, getTasksList } from './tasksGateway.js';
+
+export const onCreateTask = () => {
+  const taskTitleInputElem = document.querySelector('.task-input');
+  const text = taskTitleInputElem.value;
+  if (!text) {
+    return;
+  }
+  taskTitleInputElem.value = '';
+
+  const newTask = {
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+  };
+  createTask(newTask)
+    .then(() => getTasksList())
+    .then((newTasksList) => {
+      setItem('tasksList', newTasksList);
+      renderTasks();
+    });
+};
+
+// 1. prepare data
+// 2. write data to data base
+// 3. read new data from server
+// 4. save new data to front-end srorage
+// 5. update UI based on new data
